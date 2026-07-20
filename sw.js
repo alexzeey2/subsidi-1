40ea1876c18db3c5d9b3fd66b393c9ed42c0d50e
alexzeey2 <151472189+alexzeey2@users.noreply.github.com>
2026-07-20 09:32:32 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'subsidi-v2';
+const CACHE = 'subsidi-v3';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./'])).then(() => self.skipWaiting()));
 });
